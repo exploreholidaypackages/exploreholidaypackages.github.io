a5057fefdbee3d392ae28b0b72c066717e4440c7
exploreholidaypackages <exploreholidaypackages@gmail.com>
2015-08-08 02:45:42 +0530

    updated google analytics

--- javascripts/google-analyitcs.js
+++ javascripts/google-analyitcs.js
@@ -2,9 +2,6 @@
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  
-  ga('require', 'displayfeatures');
-  ga('create', 'UA-45813188-1', 'data-analytics.github.io');
-  ga('require', 'linkid', 'linkid.js');
-  ga('send', 'pageview');
 
+  ga('create', 'UA-66093643-1', 'auto');
+  ga('send', 'pageview');
